26249fb813801d59e183c17faaeab617a503a8c1
Miguel Chirinos <miguel.chirinos.contractor@macmillan.com>
2020-07-24 17:18:08 -0300

    who section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import classes from './About.module.css'
+import whoImage from '../../assets/images/who.jpg'
+
 
 const About = () => {
   return (
-    <section>
-      Who
+    <section className={classes.About}>
+      <div className={classes.Photo}>
+        <img src={whoImage} alt="Who"/>
+      </div>
+      <div className={classes.Description}>
+        <div className={classes.Title}>
+          Quién
+        </div>
+        <div className={classes.Text}>
+          Bajista, pianista y sonidista, nacido en Buenos Aires en 1990. 
+          Apasionado por la música y la programación. 
+          Aficionado del MIDI.
+        </div>
+      </div>
     </section>
   )
 }
