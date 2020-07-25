0072f1f1cc6f9353095a1c1f230720b8e27a03aa
migueSNM <miguelmchirinos@gmail.com>
2020-07-25 02:12:53 -0300

    work card styling

--- src/components/Works/WorkCard/WorkCard.js
+++ src/components/Works/WorkCard/WorkCard.js
@@ -5,16 +5,22 @@ const WorkCard = (props) => {
 
   const style = {
     backgroundImage: `url("${props.backgroundImage}")`
-
   }
 
   return (
     <div className={classes.WorkCard} style={style}>
-      <div className={classes.Title}>
-        {props.title}
-      </div>
-      <div>
-        Texto
+      <div className={classes.TransBox}>
+        <div className={classes.Title}>
+          {props.title}
+        </div>
+        <div className={classes.Text}>
+          <div className={classes.Primary}>
+            {props.primaryText}
+          </div>
+          <div className={classes.Secondary}>
+            {props.secondaryText}
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -8,9 +8,24 @@ import worksPhoto3 from '../../assets/images/works3.jpg'
 const Works = () => {
   return (
     <section className={classes.Works}>
-      <WorkCard title='FUSO' backgroundImage={worksPhoto1}/>
-      <WorkCard title='PAIMUN' backgroundImage={worksPhoto2}/>
-      <WorkCard title='SOFAR' backgroundImage={worksPhoto3}/>
+      <WorkCard 
+        title='FUSO' 
+        backgroundImage={worksPhoto1} 
+        primaryText='Funk y Progresivo'
+        secondaryText='Bajista desde 2017'
+      />
+      <WorkCard 
+        title='PAIMUN' 
+        backgroundImage={worksPhoto2}
+        primaryText='Folk Rock'
+        secondaryText='Bajista desde 2019'
+      />
+      <WorkCard 
+        title='SOFAR' 
+        backgroundImage={worksPhoto3}
+        primaryText='Eventos musicales en livings'
+        secondaryText='Sonidista desde 2017'
+      />
     </section>
   )
 }
