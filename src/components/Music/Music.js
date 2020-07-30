1e2d873048d59b0d24ffc33294677f8ee48e9fc6
Miguel Chirinos <miguel.chirinos.contractor@macmillan.com>
2020-07-30 09:45:44 -0300

    use soundcloud playlist

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import classes from './Music.module.css'
-import MusicCard from './MusicCard/MusicCard'
 import heroImage from '../../assets/images/hero2.jpg'
-import musicPhoto1 from '../../assets/images/music1.jpg'
-import musicPhoto2 from '../../assets/images/music2.jpg'
-import musicPhoto3 from '../../assets/images/music3.jpg'
-import musicPhoto4 from '../../assets/images/music4.jpg'
+import ReactPlayer from 'react-player'
 
 const Music = () => {
   return (
@@ -23,24 +19,14 @@ const Music = () => {
       <div className={classes.Title}>
         Por qué
       </div>
-      <div className={classes.MusicCards}>
-        <MusicCard 
-          backgroundImage={musicPhoto1}
-          url='https://soundcloud.com/migue-chirinos/odise'
-        />
-        <MusicCard 
-          backgroundImage={musicPhoto2}
-          url='https://soundcloud.com/migue-chirinos/sativa'
-        />
-        <MusicCard 
-          backgroundImage={musicPhoto3}
-          url='https://soundcloud.com/migue-chirinos/spaceshooter-main-theme'
-        />
-        <MusicCard 
-          backgroundImage={musicPhoto4}
-          url='https://soundcloud.com/migue-chirinos/combat-music-for-videogames'
-        />
-      </div>
+      <ReactPlayer 
+        url='https://soundcloud.com/migue-chirinos'
+        width='50%'
+        style={{
+          marginRight: 0,
+          marginLeft: 'auto'
+        }}
+      />
     </section>
   )
 }
