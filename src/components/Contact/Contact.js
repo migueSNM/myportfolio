a0ec0000c878acf4b05478f3a17538c745b78b9f
Miguel Chirinos <miguel.chirinos.contractor@macmillan.com>
2020-07-31 14:45:05 -0300

    contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import classes from './Contact.module.css'
+import { SocialIcon } from 'react-social-icons';
 
 const Contact = () => {
   return (
-    <footer>
-      Where
+    <footer className={classes.Contact} id="contact">
+      <SocialIcon url="https://twitter.com/miguechiri"/>
+      <SocialIcon url="https://facebook.com/migue.chirinos"/>
+      <SocialIcon url="https://instagram.com/migue.chirinos/"/>
+      <SocialIcon url="https://soundcloud.com/migue-chirinos" />
+      <SocialIcon url="https://youtube.com/channel/UC70ZyKpWqdFiGwiJrBiB1SA" />
     </footer>
   )
 }
