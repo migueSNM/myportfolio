2ef2a83d4b8b72bc463aaf688e1a814487e5f977
Miguel Chirinos <miguel.chirinos.contractor@macmillan.com>
2020-08-04 12:24:46 -0300

    contact style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,13 +3,19 @@ import classes from './Contact.module.css'
 import { SocialIcon } from 'react-social-icons';
 
 const Contact = () => {
+  var d = new Date()
+  const currentYear = d.getFullYear()
+
   return (
     <footer className={classes.Contact} id="contact">
-      <SocialIcon url="https://twitter.com/miguechiri"/>
-      <SocialIcon url="https://facebook.com/migue.chirinos"/>
-      <SocialIcon url="https://instagram.com/migue.chirinos/"/>
-      <SocialIcon url="https://soundcloud.com/migue-chirinos" />
-      <SocialIcon url="https://youtube.com/channel/UC70ZyKpWqdFiGwiJrBiB1SA" />
+      <div className={classes.Social}>
+        <SocialIcon url="https://twitter.com/miguechiri"/>
+        <SocialIcon url="https://facebook.com/migue.chirinos"/>
+        <SocialIcon url="https://instagram.com/migue.chirinos/"/>
+        <SocialIcon url="https://soundcloud.com/migue-chirinos" />
+        <SocialIcon url="https://youtube.com/channel/UC70ZyKpWqdFiGwiJrBiB1SA" />
+      </div>
+      <small>&copy; Copyright {currentYear}, Miguel Chirinos</small>
     </footer>
   )
 }
