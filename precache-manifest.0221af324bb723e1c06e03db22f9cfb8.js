cc970a21f8996a5e13d9a2605b4785cd875ba00d
Miguel Chirinos <miguel.chirinos.contractor@macmillan.com>
2020-08-10 11:09:10 -0300

    Updates

---
+++ precache-manifest.0221af324bb723e1c06e03db22f9cfb8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e6907b4be1149634e8af0552568eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e677c74b3f260e495469",
+    "url": "/static/css/main.1b6159f9.chunk.css"
+  },
+  {
+    "revision": "a7465c80b6e3ea788c22",
+    "url": "/static/js/2.5a1e9292.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/static/js/2.5a1e9292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e677c74b3f260e495469",
+    "url": "/static/js/main.15047087.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "d0ab990e3ce068c05a65c815e7ebedd8",
+    "url": "/static/media/hero1.d0ab990e.jpg"
+  },
+  {
+    "revision": "5145fe36849f4a82937653b819654ea5",
+    "url": "/static/media/music1.5145fe36.jpg"
+  },
+  {
+    "revision": "c2ac747809fe8317b6a4fbb775388dca",
+    "url": "/static/media/rune-logo.c2ac7478.png"
+  },
+  {
+    "revision": "439f4047aceea98e985b72b9bb4b604e",
+    "url": "/static/media/who.439f4047.jpg"
+  },
+  {
+    "revision": "6b6485b187b26af5c1e69bb80c3d5ded",
+    "url": "/static/media/works1.6b6485b1.jpg"
+  },
+  {
+    "revision": "50056fb73cc25a2de33141616540848d",
+    "url": "/static/media/works2.50056fb7.jpg"
+  },
+  {
+    "revision": "671a9f0273c583d9dbbbfcffb855e4f1",
+    "url": "/static/media/works3.671a9f02.jpg"
+  }
+]);
